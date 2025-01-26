45906e951e1b34a6b357eea6a879ecee88ec38ab
Sah test <sahar-ilya@mail.ru>
2025-01-26 18:10:03 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
